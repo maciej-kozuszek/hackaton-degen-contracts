69999cb51b9fa219ce9dde875d660336f90a1d7b
karczu <oskar.karcz@rumblefish.pl>
2024-02-17 19:05:38 +0100

    hotfix: remove unused data

--- contracts/StableCoin.sol
+++ contracts/StableCoin.sol
@@ -4,12 +4,6 @@ pragma solidity ^0.8.18;
 import {ERC20, ERC20Permit} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
 
 contract StableCoin is ERC20Permit {
-    // signature data
-    bytes32 private constant _INVEST_TYPEHASH =
-        keccak256("InvestmentData(address investor,uint256 amountIn,uint256 amountOut,bytes32 salt)");
-    string private constant _DOMAIN_NAME = "Mosaico";
-    string private constant _DOMAIN_VERSION = "0.1.0";
-
     constructor() ERC20("TestStableCoin", "TSC") ERC20Permit("TestStableCoin") {
         _mint(msg.sender, 10_000_000 * 10 ** decimals());
     }
