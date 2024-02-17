41da6b35226ce4b969283df92f1db1e31c030afc
Rskrzypczyk <roger.skrzypczyk@rumblefish.pl>
2024-02-17 12:14:09 +0100

    add abstract account

--- deploy/0_deploy.ts
+++ deploy/0_deploy.ts
@@ -1,3 +1,4 @@
+import { loadConfig } from 'config'
 import { DeployFunction } from 'hardhat-deploy/types'
 import { HardhatRuntimeEnvironment } from 'hardhat/types'
 
@@ -5,12 +6,26 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
     const { deployments, getNamedAccounts } = hre
     const { deployer } = await getNamedAccounts()
     const { deploy } = deployments
+
+    const config = loadConfig(hre.network.name)
+
+    const lightAccFactory = await deploy('LightAccountFactory', {
+        from: deployer,
+        log: true,
+        args: [config.entryPoint],
+    })
+
     const stable = await deploy('StableCoin', {
         from: deployer,
         log: true,
     })
 
     const equityToken = await deploy('EquityToken', {
+        from: deployer,
+        log: true,
+    })
+
+    const tok = await deploy('TOK', {
         from: deployer,
         log: true,
     })
--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,10 +1,16 @@
 import '@openzeppelin/hardhat-upgrades'
+
+import dotenv from 'dotenv'
+
 import { HardhatUserConfig } from 'hardhat/config'
+import PolygonTestnetNetwork from './config/polygon-testnet.json'
 
 import '@nomicfoundation/hardhat-verify'
 import '@nomiclabs/hardhat-ethers'
 import '@typechain/hardhat'
 import 'hardhat-deploy'
+
+dotenv.config()
 
 const config: HardhatUserConfig = {
     solidity: '0.8.18',
@@ -15,6 +21,15 @@ const config: HardhatUserConfig = {
     namedAccounts: {
         deployer: {
             default: 0,
+        },
+    },
+    networks: {
+        [PolygonTestnetNetwork.network]: {
+            chainId: Number(PolygonTestnetNetwork.chainId),
+            url: PolygonTestnetNetwork.rpcUrl,
+            throwOnCallFailures: true,
+            accounts: [process.env.DEPLOYER as string],
+            saveDeployments: true,
         },
     },
 }
